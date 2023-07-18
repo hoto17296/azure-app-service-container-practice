81f987746622fdd6dc554589d269ee416cc2c876
Yuki Ishikawa <hoto@y32.jp>
2023-07-18 18:40:48 +0900

    add mui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@mui/material'
 import { getUserInfo } from './lib/auth'
 
 function App() {
@@ -5,9 +6,9 @@ function App() {
   if (userInfo === undefined) return <p>loading...</p>
 
   return (
-    <>
+    <Container maxWidth="md">
       <h1>Hello, {userInfo.name} !</h1>
-    </>
+    </Container>
   )
 }
 
